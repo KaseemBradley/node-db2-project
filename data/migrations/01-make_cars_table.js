2d9b80c9ad4c330f02677dd4740054996e3c8577
Kaseem Bradley <kaseembradley@Meryls-phone.localdomain>
2021-06-08 17:15:26 -0400

    scripts for knex migrate up and down

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -3,6 +3,11 @@ exports.up = function (knex) {
   return knex.schema.createTable("cars", (table) => {
     table.increments("car_id"); //creates the primary key
     table.text("vin", 128).unique().notNullable();
+    table.text("make").notNullable();
+    table.text("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.text("title").nullable();
+    table.text("transmission").nullable();
   });
 };
 
