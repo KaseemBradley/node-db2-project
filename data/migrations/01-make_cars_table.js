adc5de8ed7e33d615853e5b3931588f859e9348b
Kaseem Bradley <kaseembradley@Meryls-phone.localdomain>
2021-06-08 16:44:31 -0400

    began database schema including cars table with vin coulum

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,11 @@
 // DO YOUR MAGIC
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (table) => {
+    table.increments("car_id"); //creates the primary key
+    table.text("vin", 128).unique().notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
